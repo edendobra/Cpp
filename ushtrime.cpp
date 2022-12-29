f76e5028d88251b74d9e8c50871ede4e25d2178c
edendobra <edendobra031@gmail.com>
2022-12-29 12:51:32 +0100

    e kom shtu emrin

---
+++ ushtrime.cpp
@@ -0,0 +1,37 @@
+// #include <iostream>
+// using namespace std;
+// bool eshteTek(int numri){
+//     return numri % 2 != 0;
+// }
+// int mbledhCift(int totali);
+// int main(){
+//         int totali;
+//         cout << "Shkruaj sa numra deshiron: ";
+//         cin >> totali;
+//         int shuma = mbledhCift(totali);
+//         cout << "Shuma e numrave eshte: " << shuma <<endl;
+//         return 0;
+// }
+// int mbledhCift(int totali){
+//     int numri;
+//     int shuma = 0;
+//     for (int i = 0; i < totali; i++)
+//     {
+//             cout << "Jepe numrin (" << i + 1;
+//             cin >> numri;
+//             if (!eshteTek(numri))
+//             {
+//                 shuma += numri;
+//             }
+            
+//     }
+//     return shuma;
+// }
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "eden";
+    return 0;
+}
