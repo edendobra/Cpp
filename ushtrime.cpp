1eef6a5b677ca2907a842136fb3c48176c3f75d9
edendobra <edendobra031@gmail.com>
2022-12-31 13:18:39 +0100

    only for a ' ' - tried 1,2k solutions

--- ushtrime.cpp
+++ ushtrime.cpp
@@ -65,9 +65,26 @@
 #include <string.h>
 using namespace std;
 
+string defangIPaddr(string address) {
+        string eden = "";
+        for (int i=0; i< address.length();i++){
+            if (address.at(i) == '.')   
+            {
+                eden += "[.]";
+            } else {
+                eden += address.at(i);
+            }
+            
+        }
+        return eden;
+        
+    }
+
 int main(){
 
-        
+        string address = "123.12.12.1";
+        string result = defangIPaddr(address);
+        cout << result;
 
         return 0;
 }
