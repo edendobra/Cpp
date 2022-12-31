43f593f60b576a6ae96ed0940c653c145c8f4318
edendobra <edendobra031@gmail.com>
2022-12-31 13:18:41 +0100

    hellno

---
+++ 1108.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+string defangIPaddr(string address) {
+        string eden = "";
+        for (int i=0; i< address.length();i++){
+            if (address.at(i) == '.')   
+            {
+                eden += "[.]";
+            } else {
+                eden += address.at(i);
+            }
+            
+        }
+        return eden;
+        
+    }
+
+int main(){
+
+     string address = "1.1.1.1";
+        string result = defangIPaddr(address);
+        cout << result;
+
+        return 0;
+}
