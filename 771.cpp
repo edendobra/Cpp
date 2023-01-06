ef2f33084eafda498e7e599e3e5555df54a6e433
edendobra <edendobra031@gmail.com>
2023-01-06 13:21:19 +0100

    those're making me addicted

---
+++ 771.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int numJewelsInStones(string jewels, string stones) {
+    int lengjew = jewels.size();
+    int lengsto = stones.size();
+    int count = 0;
+
+    for (int i = 0; i < lengjew; i++)
+    {
+        for (int j = 0; j < lengsto; j++)
+        {
+            if (jewels[i] == stones[j])
+            {
+                count++;
+            }
+            
+        }
+        
+    }
+    return count;
+            }
+
+int main(){
+    string jewels = "aA";
+    string stones = "aAAbbbb";
+
+    int result = numJewelsInStones(jewels, stones);
+    cout << "Number of stones that are jewels is: " << result;
+    return 0;
+}
